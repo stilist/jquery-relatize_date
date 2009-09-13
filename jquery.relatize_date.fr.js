7395999cbe2e822cbbd4714623f1114598c6424e
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2009-09-14 00:31:07 +0200

    french translation

---
+++ jquery.relatize_date.fr.js
@@ -0,0 +1,12 @@
+if(typeof $relatizeDateTranslation == "undefined")
+  $relatizeDateTranslation = {};
+
+$relatizeDateTranslation.fr = [
+  'il y a moins d\'une minute',
+  'il y a environ une minute',
+  'il y a %d minutes',
+  'il y a environ une heure',
+  'il y a environ %d heures',
+  'il y a 1 jour',
+  'il y a %d jours'
+];
