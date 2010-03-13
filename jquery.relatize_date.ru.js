6fc3cacaa71a84c198d246d491d1accd18dc454f
myfreeweb <me@myfreeweb.ru>
2010-03-13 16:07:22 +0300

    Russian language added

---
+++ jquery.relatize_date.ru.js
@@ -0,0 +1,12 @@
+if(typeof $relatizeDateTranslation == "undefined") 
+  $relatizeDateTranslation = {};
+
+$relatizeDateTranslation.en = [
+  "меньше минуты назад",
+  "около минуты назад",
+  "%d минут(ы) назад",
+  "около часа назад",
+  "около %d часов назад",
+  "1 день назад",
+  "%d дней назад"
+];
