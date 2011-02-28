d9200ef00513ba47206524fc83a0ffe1cb655437
stilist <stilist@ratafia.info>
2011-02-27 21:48:51 -0800

    Significant rewrite, but the code should operate almost the same: split date/number methods out into object-type-specific language extension files; add error handling; simplify; make code that checks browser locale settings actually work.

---
+++ date.js
@@ -0,0 +1,143 @@
+// Return the distance of time in words between two `Date`s, e.g.
+// "5 days ago", "менее минуты назад".
+//
+// Options:
+//   `end`: `Date`
+//   `includeTime`: `Boolean`; used if date is in the past 2-5 days
+//   `translation`: `Object`; relatizeDate translation
+if (!Date.prototype.distanceOfTimeInWords) {
+	Date.prototype.distanceOfTimeInWords =
+			function (end, includeTime, translation) {
+		var start = this,
+				minute = 60,
+				hour = minute * 60,
+				day = hour * 24,
+				n_days = 0,
+				delta = 0,
+				date = "",
+				time = "",
+				format = "";
+
+		if (Date !== start.constructor || Date !== end.constructor) {
+			throw new TypeError();
+		} else {
+			delta = parseInt(((end.getTime() - start.getTime()) / 1000), 10);
+
+			// Less Than a Minute
+			if (minute > delta) {
+				date = translation.ltm;
+			// ABout n Minutes (1-2 minutes)
+			} else if ((minute * 2) > delta) {
+				date = translation.abm;
+			// 2-45 Minutes
+			} else if ((45 * minute) > delta) {
+				date = translation.m.replace("%d",
+						parseInt((delta / minute), 10));
+			// 1-2 Hours
+			} else if ((hour * 2) > delta) {
+				date = translation.h;
+			// ABout n Hours (2-23 hours)
+			} else if ((24 * hour) > delta) {
+				date = translation.abh.replace("%d", parseInt(hour, 10));
+			// 1-2 days
+			} else if ((48 * hour) > delta) {
+				// 12-hour clock
+				if (12 === translation.default_time_fmt) {
+					time = (start.getHours() + 12) % 12 + ":" +
+							start.getMinutes().pad("0", 2) + " " +
+							(start.getHours() > 12 ? "PM" : "AM");
+				// 24-hour clock
+				} else {
+					time = start.getHours() + ":" + start.getMinutes();
+				}
+
+				// yesterDay At
+				date = translation.d + " " + translation.at + " " + time;
+			} else {
+				n_days = parseInt((delta / day), 10).toString(10); // base 10
+
+				// 5+ days
+				if (5 < n_days) {
+					format = "%B %d, %Y";
+
+					if (includeTime) {
+						format += " %I:%M %p";
+					}
+
+					date = start.strftime(format, translation);
+				// 2-5 days
+				} else {
+					if ("undefined" !== typeof translation.shortds
+							&& "undefined" !== typeof translation.shortds[n_days - 2]) {
+						date = translation.shortds[n_days - 2];
+					} else {
+						date = translation.ds.replace("%d", n_days);
+					}
+				}
+			}
+
+			return date;
+		}
+	};
+}
+
+if (!Date.prototype.isValid) {
+	Date.prototype.isValid = function() {
+		if (Date !== this.constructor) {
+			throw new TypeError();
+		} else {
+			return !isNaN(this) && 0 < this;
+		}
+	};
+}
+
+if (!Date.prototype.strftime) {
+	Date.prototype.strftime = function (format, translation) {
+		var date = this,
+				day = date.getDay(),
+				month = date.getMonth(),
+				hours = date.getHours(),
+				minutes = date.getMinutes();
+
+		if (date.isValid()) {
+			return format.replace(/\%([aAbBcdHImMpSwyY])/g, function (key) {
+				switch(key[1]) {
+					case "a": return translation.shortDays[day];
+					case "A": return translation.days[day];
+					case "b": return translation.shortMonths[month];
+					case "B": return translation.months[month];
+					case "c": return date.toString();
+					case "d": return date.getDate().pad("0", 2);
+					case "H": return hours.pad("0", 2);
+					case "I": return ((hours + 12) % 12).pad("0", 2);
+					case "m": return (month + 1).pad("0", 2);
+					case "M": return minutes.pad("0", 2);
+					case "p": return hours > 12 ? "PM" : "AM";
+					case "S": return date.getSeconds().pad("0", 2);
+					case "w": return day;
+					case "y": return (date.getFullYear() % 100).pad("0", 2);
+					case "Y": return date.getFullYear().toString();
+				}
+			});
+		} else {
+			// xxx not sure what the appropriate return value is here (/jordan)
+			return date;
+		}
+	};
+}
+
+// Convenience method for calling `distanceOfTimeInWords` relative to the
+// current timestamp.
+//
+// Options:
+//   `includeTime`: `Boolean`; used if date is in the past 2-5 days
+//   `translation`: `Object`; relatizeDate translation
+if (!Date.prototype.timeAgoInWords) {
+	Date.prototype.timeAgoInWords = function (includeTime, translation) {
+		if (Date !== this.constructor) {
+			throw new TypeError();
+		} else if (this.isValid()) {
+			return this.distanceOfTimeInWords(new Date(), includeTime, translation);
+		}
+	};
+}
--- jquery.relatize_date.js
+++ jquery.relatize_date.js
@@ -1,114 +1,61 @@
 // Almost all credit goes to Rick Olson.
-(function($) {
-  $.fn.relatizeDate = function(params) {
-    if(typeof $.relatizeDate.language == "undefined")
-      if(typeof params == "undefined")
-        for(key in $relatizeDateTranslation)
-          var language = key;
-      else if(typeof params.availableLanguages == "object") {
-        var currentLanguage = (typeof navigator.browserLanguage == "undefined") ? 
-          navigator.language : navigator.browserLanguage;
-        currentLanguage.toLowerCase().match(/((\w+)-\w+)/);
-        if($relatizeDateTranslation[RegExp.$1])
-          var language = RegExp.$1;
-        else if(RegExp.$1 != RegExp.$2 && $relatizeDateTranslation[RegExp.$2])
-          var language = RegExp.$2;
-        else
-          var language = params.defaultLanguage;
-      } else
-        var language = params.defaultLanguage;
-    
-    $.relatizeDate.translation = $relatizeDateTranslation[language];
-    if (typeof $.relatizeDate.translation.default_date_fmt != "undefined") dfmt = $.relatizeDate.translation.default_date_fmt;
-    else dfmt = '%B %d, %Y %I:%M %p';
-    return $(this).each(function() {
-      if (typeof params.titleize != "undefined" && params.titleize == true)
-	  $(this).attr('title', $.relatizeDate.strftime(new Date($(this).text()), dfmt));
-      $(this).text($.relatizeDate(this));
-    });
-  };
+(function ($) {
+	$.fn.relatizeDate = function (options) {
+		// Options:
+		//   `defaultLanguage`: locale name (e.g. `"sv-SV"`, `"ru"`); used if
+		//      we're unable to determine user's preferred locale or there's no
+		//      translation for the preferred locale
+		//   `titleize`: boolean; whether to set `title` attribute of `$(this)` to
+		//      original timestamp text in `$(this)`
+		var settings = $.extend({
+					defaultLanguage: "",
+					titleize: false
+				}, options),
+				format = "",
+				locale = "",
+				preferred_language = "",
+				preferred_locales = [],
+				translation = {};
 
-  $.relatizeDate = function(element) {
-    return $.relatizeDate.timeAgoInWords(new Date($(element).text()));
-  };
+		if ("undefined" === typeof navigator.browserLanguage) {
+			preferred_language = navigator.language;
+		} else {
+			preferred_language = navigator.browserLanguage;
+		}
+		// `"en-us"` => `["en_us", "en", "-us"]`
+		// `"en"` => `["en", "en", undefined]`
+		preferred_locales = preferred_language.toLowerCase().match(/(\w+)(\-\w+)/);
 
-  $.extend($.relatizeDate, {
-    /**
-     * Given a formatted string, replace the necessary items and return.
-     * Example: Time.now().strftime("%B %d, %Y") => February 11, 2008
-     * @param {String} format The formatted string used to format the results
-     */
-    strftime: function(date, format) {
-      var day = date.getDay(), month = date.getMonth(),
-      hours = date.getHours(), minutes = date.getMinutes(),
-      translation = $.relatizeDate.translation;
+		// e.g. `"fr-fr"`
+		if ($relatizeDateTranslation[preferred_locales[0]]) {
+			locale = preferred_locales[0];
+		// e.g. `"fr"`
+		} else if ($relatizeDateTranslation[preferred_locales[1]]) {
+			locale = preferred_locales[1];
+		} else {
+			locale = settings.defaultLanguage;
+		}
 
-      function pad(num) { 
-        var string = num.toString(10);
-        return new Array((2 - string.length) + 1).join('0') + string;
-      };
+		translation = $relatizeDateTranslation[locale];
+		if ("undefined" !== typeof translation) {
+			if ("undefined" !== typeof translation.default_date_fmt) {
+				format = translation.default_date_fmt;
+			} else {
+				format = "%B %d, %Y %I:%M %p";
+			}
+		}
 
-      return format.replace(/\%([aAbBcdHImMpSwyY])/g, function(part) {
-        switch(part[1]) {
-          case 'a': return translation.shortDays[day]; break;
-          case 'A': return translation.days[day]; break;
-          case 'b': return translation.shortMonths[month]; break;
-          case 'B': return translation.months[month]; break;
-          case 'c': return date.toString(); break;
-          case 'd': return pad(date.getDate()); break;
-          case 'H': return pad(hours); break;
-          case 'I': return pad((hours + 12) % 12); break;
-          case 'm': return pad(month + 1); break;
-          case 'M': return pad(minutes); break;
-          case 'p': return hours > 12 ? 'PM' : 'AM'; break;
-          case 'S': return pad(date.getSeconds()); break;
-          case 'w': return day; break;
-          case 'y': return pad(date.getFullYear() % 100); break;
-          case 'Y': return date.getFullYear().toString(); break;
-        }
-      });
-    },
-  
-    timeAgoInWords: function(targetDate, includeTime) {
-      return $.relatizeDate.distanceOfTimeInWords(targetDate, new Date(), includeTime);
-    },
-  
-    /**
-     * Return the distance of time in words between two Date's
-     * Example: '5 days ago', 'about an hour ago'
-     * @param {Date} fromTime The start date to use in the calculation
-     * @param {Date} toTime The end date to use in the calculation
-     * @param {Boolean} Include the time in the output
-     */
-    distanceOfTimeInWords: function(fromTime, toTime, includeTime) {
-      var delta = parseInt((toTime.getTime() - fromTime.getTime()) / 1000, 10);
-      var translation = $.relatizeDate.translation;
-      if (delta < 60) {
-          return translation.ltm; // (L)ess (T)han a (M)inute
-      } else if (delta < 120) {
-          return translation.abm; // (AB)out a (M)inute
-      } else if (delta < (45*60)) {
-          return translation.m.replace("%d", parseInt(delta / 60, 10)); // (M)inute
-      } else if (delta < (120*60)) {
-          return translation.h; // (H)our
-      } else if (delta < (24*60*60)) {
-          return translation.abh.replace("%d", parseInt(delta / 3600, 10)); // (AB)out a (H)our
-      } else if (delta < (48*60*60)) {
-        if (typeof translation.default_time_fmt != 'undefined' && translation.default_time_fmt == 12) time = (fromTime.getHours() + 12) % 12 + ':' + fromTime.getMinutes() + ' ' + (fromTime.getHours() > 12 ? 'PM' : 'AM');
-        else time = fromTime.getHours() + ':' + fromTime.getMinutes();
-          return translation.d + ' ' + translation.at + ' ' + time; // yester(D)ay
-      } else {
-        var days = (parseInt(delta / 86400, 10)).toString();
-        if (days > 5) {
-          var fmt  = '%B %d, %Y';
-          if (includeTime) fmt += ' %I:%M %p';
-          return $.relatizeDate.strftime(fromTime, fmt);
-        } else
-          if (typeof(translation.shortds) != 'undefined' &&
-	      typeof(translation.shortds[days-2]) != 'undefined')
-	      return translation.shortds[days-2]; // less than 5 days
-          else return translation.ds.replace("%d", days); // (D)ay(S)
-      }
-    }
-  });
+		return this.each(function () {
+			var date = new Date($(this).text());
+
+			// If the date or translation is invalid, leave the text as-is.
+			if (date.isValid() && "undefined" !== typeof translation) {
+				if (settings.titleize) {
+					$(this).attr("title", date.strftime(format, translation));
+				}
+
+				$(this).text(date.timeAgoInWords(false, translation));
+			}
+		});
+	};
 })(jQuery);
